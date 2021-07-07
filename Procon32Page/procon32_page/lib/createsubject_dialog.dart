bb0addc334508e7144659f97086b3346a9e60954
sthairno <45668272+sthairno@users.noreply.github.com>
2021-07-07 22:27:04 +0900

    リファクタリング, ピースの並べ替えを実装

--- Procon32Page/procon32_page/lib/createsubject_dialog.dart
+++ Procon32Page/procon32_page/lib/createsubject_dialog.dart
@@ -8,6 +8,14 @@ import 'package:file_picker/file_picker.dart';
 import 'package:image/image.dart' as imglib;
 import 'package:cool_stepper/cool_stepper.dart';
 
+class _PeaceData {
+  int dispIdx;
+
+  int rotAngle;
+
+  _PeaceData({required this.dispIdx, this.rotAngle = 0}) {}
+}
+
 class _SubjectData {
   String? displayName = null;
 
@@ -21,7 +29,168 @@ class _SubjectData {
 
   int? peaceCntY = null;
 
-  imglib.Image? image = null;
+  imglib.Image? baseImage = null;
+
+  List<_PeaceData> peaces = [];
+}
+
+class _PeaceTmpData {
+  Uint8List thumbnail;
+
+  Key widgetKey;
+
+  _PeaceTmpData({required this.thumbnail}) : widgetKey = UniqueKey() {}
+}
+
+class PuzzleEditor extends StatefulWidget {
+  const PuzzleEditor({Key? key, required this.data, this.visible = true})
+      : super(key: key);
+
+  final _SubjectData data;
+
+  final bool visible;
+
+  @override
+  _PuzzleEditorState createState() => _PuzzleEditorState();
+}
+
+class _PuzzleEditorState extends State<PuzzleEditor> {
+  late List<_PeaceTmpData> _peaceTmpData;
+
+  bool _initialized = false;
+
+  void _deleteData() {
+    if (!_initialized) {
+      return;
+    }
+    _peaceTmpData = [];
+    widget.data.peaces = [];
+
+    _initialized = false;
+  }
+
+  void _initData() {
+    if (_initialized) {
+      return;
+    }
+    int aryLength = widget.data.peaceCntX! * widget.data.peaceCntY!;
+    int peaceSize = widget.data.baseImage!.width ~/ widget.data.peaceCntX!;
+    int thumbnailPeaceSize = 400 ~/ widget.data.peaceCntX!;
+    bool requiredResize = peaceSize > thumbnailPeaceSize;
+
+    _peaceTmpData = List.generate(aryLength, (index) {
+      int x = index % widget.data.peaceCntX!;
+      int y = index ~/ widget.data.peaceCntX!;
+
+      var peaceImg = imglib.copyCrop(widget.data.baseImage!, x * peaceSize,
+          y * peaceSize, peaceSize, peaceSize);
+
+      if (requiredResize) {
+        peaceImg = imglib.copyResize(peaceImg, width: thumbnailPeaceSize);
+      }
+
+      return _PeaceTmpData(thumbnail: imglib.encodeJpg(peaceImg) as Uint8List);
+    });
+
+    widget.data.peaces =
+        List.generate(aryLength, (index) => _PeaceData(dispIdx: index));
+
+    _initialized = true;
+  }
+
+  void _updateData() {
+    if (widget.visible) {
+      _initData();
+    } else {
+      _deleteData();
+    }
+  }
+
+  Widget _buildRotButton(_PeaceData data, _PeaceTmpData tmpData) => RotatedBox(
+        quarterTurns: data.rotAngle,
+        child: Image.memory(
+          tmpData.thumbnail,
+          width: double.infinity,
+          key: tmpData.widgetKey,
+          fit: BoxFit.cover,
+        ),
+      );
+
+  Widget _buildPeaceWidget(int dispIdx) {
+    var dataIdx =
+        widget.data.peaces.indexWhere((data) => data.dispIdx == dispIdx);
+    var data = widget.data.peaces[dataIdx];
+    var tmpData = _peaceTmpData[dataIdx];
+
+    return DragTarget(
+      builder: (buildercontext, candidateData, rejectedData) {
+        return Draggable(
+            data: dataIdx,
+            feedback: Image.memory(tmpData.thumbnail),
+            child: candidateData.length > 0
+                ? Container(
+                    width: double.infinity,
+                    height: double.infinity,
+                    color: Colors.red.shade100,
+                  )
+                : dispIdx == 0
+                    ? _buildRotButton(data, tmpData)
+                    : TextButton(
+                        onPressed: () {
+                          setState(() {
+                            data.rotAngle = (data.rotAngle + 1) % 4;
+                          });
+                        },
+                        child: _buildRotButton(data, tmpData),
+                      ),
+            childWhenDragging: Container());
+      },
+      onWillAccept: (_) => true,
+      onAccept: (targetDataIdx) {
+        if (targetDataIdx is int) {
+          var targetData = widget.data.peaces[targetDataIdx];
+
+          setState(() {
+            // 交換元,交換先のピースが座標00のとき回転を戻す
+            if (data.dispIdx == 0) {
+              targetData.rotAngle = 0;
+            }
+            if (targetData.dispIdx == 0) {
+              data.rotAngle = 0;
+            }
+
+            // 交換
+            var tmp = data.dispIdx;
+            data.dispIdx = targetData.dispIdx;
+            targetData.dispIdx = tmp;
+          });
+        }
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _updateData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _updateData();
+    return _initialized
+        ? AspectRatio(
+            aspectRatio: widget.data.peaceCntX! / widget.data.peaceCntY!,
+            child: GridView.count(
+              crossAxisCount: widget.data.peaceCntX!,
+              children: List.generate(
+                widget.data.peaces.length,
+                (dispIdx) => _buildPeaceWidget(dispIdx),
+              ),
+            ),
+          )
+        : Container();
+  }
 }
 
 class CreateSubjectDialog extends StatefulWidget {
@@ -34,11 +203,11 @@ class CreateSubjectDialog extends StatefulWidget {
 class _CreateSubjectDialogState extends State<CreateSubjectDialog> {
   late _SubjectData data;
 
+  bool _isParamValid = false;
+
   imglib.Image? _selectedImage;
 
   Uint8List? _selectedImageThumbnail;
-
-  List<Uint8List> _peaceImageThumbnails = [];
 
   final _paramFormKey = GlobalKey<FormState>();
 
@@ -50,8 +219,20 @@ class _CreateSubjectDialogState extends State<CreateSubjectDialog> {
   }
 
   void _createSelectedImageThumbnail() {
-    _selectedImageThumbnail = imglib
-        .encodeJpg(imglib.copyResize(_selectedImage!, width: 400)) as Uint8List;
+    const int maxWidth = 600;
+    const int maxHeight = 400;
+
+    var scale = min(
+        maxWidth / _selectedImage!.width, maxHeight / _selectedImage!.height);
+
+    if (scale < 1) {
+      _selectedImageThumbnail = imglib.encodeJpg(imglib.copyResize(
+          _selectedImage!,
+          width: (_selectedImage!.width * scale).round(),
+          height: (_selectedImage!.height * scale).round())) as Uint8List;
+    } else {
+      _selectedImageThumbnail = imglib.encodeJpg(_selectedImage!) as Uint8List;
+    }
   }
 
   void _createClippedImage() {
@@ -59,31 +240,12 @@ class _CreateSubjectDialogState extends State<CreateSubjectDialog> {
         _selectedImage!.height ~/ data.peaceCntY!);
     int imgWidth = peaceSize * data.peaceCntX!;
     int imgHeight = peaceSize * data.peaceCntY!;
-    setState(() {});
-    data.image = imglib.copyCrop(
+    data.baseImage = imglib.copyCrop(
         _selectedImage!,
         (_selectedImage!.width - imgWidth) ~/ 2,
         (_selectedImage!.height - imgHeight) ~/ 2,
         imgWidth,
         imgHeight);
-  }
-
-  void _createPeaceImages() {
-    const thumbnailSize = 100;
-
-    int peaceSize = data.image!.width ~/ data.peaceCntX!;
-    bool needsResize = peaceSize > thumbnailSize;
-    _peaceImageThumbnails = [];
-    for (int y = 0; y < data.peaceCntY!; y++) {
-      for (int x = 0; x < data.peaceCntX!; x++) {
-        var peaceImg = imglib.copyCrop(
-            data.image!, x * peaceSize, y * peaceSize, peaceSize, peaceSize);
-        if (needsResize) {
-          peaceImg = imglib.copyResize(peaceImg, width: thumbnailSize);
-        }
-        _peaceImageThumbnails.add(imglib.encodeJpg(peaceImg) as Uint8List);
-      }
-    }
   }
 
   // パラメーター設定
@@ -113,6 +275,7 @@ class _CreateSubjectDialogState extends State<CreateSubjectDialog> {
                 setState(() {
                   data.peaceCntX = value as int?;
                 });
+                _isParamValid = false;
               },
               validator: (value) => value == null ? "選択してください" : null,
               decoration: InputDecoration(labelText: "横分割数"),
@@ -136,6 +299,7 @@ class _CreateSubjectDialogState extends State<CreateSubjectDialog> {
                   setState(() {
                     data.peaceCntY = value as int?;
                   });
+                  _isParamValid = false;
                 }
               },
               validator: (value) => value == null ? "選択してください" : null,
@@ -175,6 +339,26 @@ class _CreateSubjectDialogState extends State<CreateSubjectDialog> {
             )
           : null;
 
+  String? _loadImage(Uint8List bytes) {
+    var image = imglib.decodeImage(bytes);
+    if (image == null) {
+      return "ファイルを読み込めませんでした";
+    }
+    if (image.width < 32 ||
+        2048 < image.width ||
+        image.height < 32 ||
+        2048 < image.height) {
+      return "画像の幅,高さは32px~2048pxである必要があります";
+    }
+    setState(() {
+      _selectedImage = image;
+      _createSelectedImageThumbnail();
+      data.baseImage = null;
+    });
+    _isParamValid = false;
+    return null;
+  }
+
   Widget _buildImageSelector() {
     return Padding(
       padding: const EdgeInsets.all(8.0),
@@ -192,22 +376,7 @@ class _CreateSubjectDialogState extends State<CreateSubjectDialog> {
               if (file.bytes == null) {
                 return "ファイルを開けませんでした";
               }
-              var image = imglib.decodeImage(file.bytes!);
-              if (image == null) {
-                return "ファイルを読み込めませんでした";
-              }
-              if (image.width < 32 ||
-                  2048 < image.width ||
-                  image.height < 32 ||
-                  2048 < image.height) {
-                return "画像の幅,高さは32px~2048pxである必要があります";
-              }
-              setState(() {
-                _selectedImage = image;
-                _createSelectedImageThumbnail();
-                _peaceImageThumbnails = [];
-              });
-              return null;
+              return _loadImage(file.bytes!);
             }();
             if (validateResult != null) {
               ScaffoldMessenger.of(context)
@@ -257,9 +426,9 @@ class _CreateSubjectDialogState extends State<CreateSubjectDialog> {
             setState(() {
               _paramFormKey.currentState!.save();
               _createClippedImage();
-              _createPeaceImages();
             });
           }
+          _isParamValid = noError;
           return noError ? null : "Error";
         },
         content: Form(
@@ -322,6 +491,16 @@ class _CreateSubjectDialogState extends State<CreateSubjectDialog> {
         ),
       );
 
+  CoolStep _buildPeaceStep() => CoolStep(
+        title: "並べ替え",
+        subtitle: "ピースをドラッグして交換、タップして90度ずつ回転することができます",
+        content: PuzzleEditor(
+          data: data,
+          visible: _isParamValid,
+        ),
+        validation: () => null,
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -339,20 +518,10 @@ class _CreateSubjectDialogState extends State<CreateSubjectDialog> {
           stepText: "",
           ofText: "/",
           finalText: "生成",
-          isHeaderEnabled: false,
         ),
         steps: [
           _buildParamStep(),
-          CoolStep(
-              title: "A",
-              subtitle: "",
-              content: Container(),
-              validation: () => null),
-          CoolStep(
-              title: "B",
-              subtitle: "",
-              content: Container(),
-              validation: () => null)
+          _buildPeaceStep(),
         ],
       ),
     );
